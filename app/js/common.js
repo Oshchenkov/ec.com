8bdd962717ad04f84a07ba6ca1f099c46d82f7e6
Oshchenkov <0639803145v@gmail.com>
2018-02-20 18:54:48 +0200

    compress img, add fontello fonts and #nav changed

--- app/js/common.js
+++ app/js/common.js
@@ -8,8 +8,8 @@ $(document).ready(function(){
   	"loop": 							true,
   	"dots": 							false,
   	"navContainerClass": "owl-nav",
-  	"autoplay": 					true,
-  	"autoplayTimeout": 		4500
+  	 "autoplay": 					true,
+  	 "autoplayTimeout": 		4500
   });
 
   // Active input in contact form
@@ -23,7 +23,7 @@ $(document).ready(function(){
 
   // Scroll to element and focus
 
-  $("[data-scroll-to]").click(function() {
+/*  $("[data-scroll-to]").click(function() {
 	  var $this = $(this),
 	      $toElement      = $this.attr('data-scroll-to'),
 	      $focusElement   = $this.attr('data-scroll-focus'),
@@ -35,6 +35,44 @@ $(document).ready(function(){
 	  }, $speed);
 	  
 	  if ($focusElement) $($focusElement).focus();
-	});
+	});*/
+
+  $('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+
+  //
 
 });
